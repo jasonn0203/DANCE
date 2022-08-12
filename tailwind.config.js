ed5c9ba30cd1906e7db9285694dfd6cba14296f2
Jason Huynh <jasonn0203@gmail.com>
2022-08-12 15:37:40 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  purge: ["./*.html"],
+  content: [],
+  theme: {
+    colors: {
+      "black-16": "#161617",
+      "gray-c0": "#C0BFC4",
+      "gray-c0-opacity": "#c0bfc480",
+      "gray-69": "#696665",
+      "purple-55": "#55549D",
+      "blue-77": "#778EA8",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
